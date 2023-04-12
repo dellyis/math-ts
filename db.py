aafc5d6407459840c5b7fbca6036f046a7251919
Dellyis <nechaev.andrysha@yandex.ru>
2023-04-12 17:51:24 +0700

    feat: migrate to another db (mongo)

--- db.py
+++ db.py
@@ -1,49 +1,45 @@
 import secrets
 
 from passlib.hash import sha256_crypt
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base
+from motor.motor_tornado import MotorClient
 
-engine = create_engine("sqlite:///data.db")
-
-Base = declarative_base()
+from config import MONGO_TOKEN
+from db_core import NiceCollection, NiceDocument, field
 
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    login = Column(String, unique=True)
-    email = Column(String)
-    password_hash = Column(String(100), nullable=False)
-    access_token = Column(String(32), unique=True, nullable=True)
-
-    @property
-    def password(self):
-        raise AttributeError("password is not a readable attribute")
-
-    @property
-    def json(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "login": self.login,
-            "email": self.email,
-        }
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = sha256_crypt.hash(password)
-
-    def verify_password(self, password):
-        return sha256_crypt.verify(password, self.password_hash)  # type: ignore
-
-    def generate_access_token(self):
-        self.access_token = secrets.token_hex(16)
-        session.commit()
+class FieldCollectionEngine(NiceCollection):
+    async def find_by_field(self, **fields):
+        for doc in self.cache.values():
+            if all(getattr(doc, field) == value for field, value in fields.items()):
+                return doc
+        data = await self.mongo_col.find_one(fields)
+        if data is None:
+            return None
+        doc = self.document_wrapper(data, self)
+        self.cache[doc.id] = doc
+        return doc
 
 
-Base.metadata.create_all(engine)
+class User(NiceDocument):
+    name: str = field()
+    login: str = field()
+    email: str = field()
+    password: str = field()
+    access_token: str = field()
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    @staticmethod
+    def generate_access_token():
+        return secrets.token_hex(16)
+
+    @staticmethod
+    def get_password_hash(password: str):
+        return sha256_crypt.hash(password)
+
+    @staticmethod
+    def verify_password(password: str, password_hash: str):
+        return sha256_crypt.verify(password, password_hash)
+
+
+cluster = MotorClient(MONGO_TOKEN)
+db = cluster["dev"]
+users = FieldCollectionEngine(db["users"], User)
