db2b07e7256b3408d47ac8b452bcf90113ceaa14
Dellyis <nechaev.andrysha@yandex.ru>
2023-04-12 17:52:42 +0700

    feat: rewrite auth decorator to new db

--- auth.py
+++ auth.py
@@ -1,17 +1,17 @@
-from flask import redirect, request
+from quart import redirect, request
 
-from db import session, User
+from db import User, users
 
 
 def auth_required(get_account: bool = False):
     def decorator(func):
         async def wrapper(*args, **kwargs):
-            token = request.cookies.get("access_token")
+            token = request.cookies.get("Authorization")
 
             if not token:
                 return redirect(f"/login?redirect_uri={request.full_path}")
 
-            account = session.query(User).filter_by(access_token=token).first()
+            account = await users.find_by_field(access_token=token)
 
             if not account:
                 return redirect(f"/login?redirect_uri={request.full_path}")
