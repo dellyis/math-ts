ebbc9898c60fd501266dc0b534b5e4cd70994465
Dellyis <nechaev.andrysha@yandex.ru>
2023-04-16 16:26:32 +0700

    feat: add some constants for frontend

---
+++ constants.py
@@ -0,0 +1,8 @@
+from enums import GameEnum
+
+
+GAMES = {
+    GameEnum.domino.value: ("domino", "Домино"),
+    GameEnum.krestiki.value: ("krestiki", "Крестики-нолики"),
+    GameEnum.bonus.value: ("bonus", "Бонусы"),
+}
