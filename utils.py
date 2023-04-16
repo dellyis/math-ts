1bf014728e84b391c97ae105a4119df56fe37327
Dellyis <nechaev.andrysha@yandex.ru>
2023-04-16 16:24:48 +0700

    feat: add few farmatters

---
+++ utils.py
@@ -0,0 +1,31 @@
+from typing import Union, List, Set, Tuple
+
+from pytils import numeral
+
+
+def format_time(time: int) -> str:
+    days, time = divmod(time, 86400)
+    hours, time = divmod(time, 3600)
+    minutes, seconds = divmod(time, 60)
+    formatted_time = []
+    if days:
+        formatted_time.append(numeral.get_plural(days, "день, дня, дней"))
+    if hours:
+        formatted_time.append(numeral.get_plural(hours, "час, часа, часов"))
+    if minutes:
+        formatted_time.append(numeral.get_plural(minutes, "минута, минуты, минут"))
+    if seconds:
+        formatted_time.append(numeral.get_plural(seconds, "секунда, секунды, секунд"))
+    return " ".join(formatted_time)
+
+
+def format_range(array: Union[List, Set, Tuple]) -> str:
+    ranges = []
+
+    for i in sorted(set(array)):
+        if not ranges or i > ranges[-1][-1] + 1:
+            ranges.append([i])
+        else:
+            ranges[-1].append(i)
+
+    return ", ".join(f"{r[0]}-{r[-1]}" if len(r) > 1 else str(r[0]) for r in ranges)
