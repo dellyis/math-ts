3edb9ab726d02163ec5c0008706870c5dfedd5c6
Dellyis <nechaev.andrysha@yandex.ru>
2023-05-05 22:44:03 +0700

    feat: add kick/selfkick and add page of team

--- blueprints/teams.py
+++ blueprints/teams.py
@@ -1,5 +1,6 @@
 from quart import Blueprint, redirect, render_template, request
 
+from argsparser import ParsedArg, url_params
 from auth import auth_required
 from db import Team, User, teams, users
 from snowflake import snowflake_generator
@@ -34,9 +35,85 @@ async def team_create_page(user: User):
 @auth_required(True)
 async def team_page(user: User, team_id: str):
     team = await teams.find(team_id)
+
+    if user.id not in team.members:
+        return await render_template(
+            "error.html", description="Вы не участник этой команды"
+        )
+
     team_members = [await users.find(member_id) for member_id in team.members]
 
-    return await render_template("team.html", team=team)
+    return await render_template(
+        "team.html",
+        team=team,
+        team_members=team_members,
+        user=user,
+    )
+
+
+@teams_bp.route("/<team_id>/kick/<member_id>")
+@auth_required(True)
+@url_params
+async def kick_member(
+    user: User, team_id: str, member_id: str, self: ParsedArg = "false"
+):
+    team = await teams.find(team_id)
+
+    if user.id not in team.members:
+        return await render_template(
+            "error.html", description="Вы не участник этой команды"
+        )
+
+    if self not in ("true", "false"):
+        return await render_template("error.html", description="incorr `self` param")
+
+    self = self == "true"
+
+    if (
+        (self and user.id != member_id)
+        or user.id != team.owner
+        or member_id == team.owner
+    ):
+        return await render_template("error.html", description="Недостаточно прав")
+
+    if (not self and user.id == member_id) or member_id == team.owner:
+        return await render_template(
+            "error.html", description="Вы не можете себя выгнать из команды"
+        )
+
+    if self:
+        async with team.command_maker() as edit_team:
+            edit_team.members.remove(member_id)
+
+        return redirect("/teams")
+
+    async with team.command_maker() as edit_team:
+        edit_team.members.remove(member_id)
+
+    return redirect(f"/teams/{team_id}")
+
+
+@teams_bp.route("/<team_id>/edit", methods=["GET", "POST"])
+@auth_required(True)
+async def team_edit_page(user: User, team_id: str):
+    team = await teams.find(team_id)
+
+    if user.id != team.owner:
+        return await render_template("error.html", description="Недостаточно прав")
+
+    if request.method == "GET":
+        return await render_template("team_edit.html", team=team)
+
+    form = await request.form
+    name = form.get("name")
+
+    if not name:
+        return await render_template("team_edit.html", err_not_filled=True)
+
+    async with team.command_maker() as edit_team:
+        edit_team.name = name[:64]
+
+    return redirect(f"/teams/{team_id}")
 
 
 @teams_bp.route("/join/<team_invite>")
