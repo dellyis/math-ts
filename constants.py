2c11d46caa0dd9b2e43cd869faa069d49199e9fa
Dellyis <nechaev.andrysha@yandex.ru>
2023-05-06 17:39:38 +0700

    feat: add `host` constant

--- constants.py
+++ constants.py
@@ -1,8 +1,15 @@
 from enums import GameEnum
 
+from config import PRODUCTION, DOMAIN
+
 
 GAMES = {
     GameEnum.domino.value: ("domino", "Домино"),
     GameEnum.krestiki.value: ("krestiki", "Крестики-нолики"),
     GameEnum.bonus.value: ("bonus", "Бонусы"),
 }
+
+if PRODUCTION:
+    HOST = f"https://{DOMAIN}"
+else:
+    HOST = "http://localhost:5000"
